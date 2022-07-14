3841ddfec9c87a991396f74500c8858f6651bce6
Tom Draper <tomjdraper1@gmail.com>
2022-07-14 18:09:28 +0100

    fix bug

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"typing/src"
+	"typing/cmd"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(src.InitialModel())
+	p := tea.NewProgram(cmd.InitialModel())
 	if err := p.Start(); err != nil {
 		fmt.Printf("Error: %v", err)
 		os.Exit(1)
