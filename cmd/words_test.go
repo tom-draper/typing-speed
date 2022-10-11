ca44d9b13cfbb60b685fe3869a72b27252bf29e3
Tom Draper <tomjdraper1@gmail.com>
2022-10-11 23:03:04 +0100

    Fix tests

--- cmd/words_test.go
+++ cmd/words_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestWikiLinks(t *testing.T) {
-	links := getWikiLinksOld()
+	links := wikiLinks()
 	seen := make(map[string]struct{})
 	for _, link := range links {
 		if _, ok := seen[link]; ok {
